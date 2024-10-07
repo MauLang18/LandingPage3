d4d9f72faf2ae9de4b996abf4ee67e6d508ccb36
MauLang18 <maulangbonilla.18@gmail.com>
2024-10-07 11:21:05 -0600

    feat: button chat with signalR

--- src/components/Bitrix24Button.jsx
+++ src/components/Bitrix24Button.jsx
@@ -1,23 +1,44 @@
 import React, { useEffect, useState } from "react";
+import useFetchIdAndUpdateSignalR from "../hooks/useFetchIdAndUpdateSignalR";
 
 const Bitrix24Button = () => {
   const [buttonLoaded, setButtonLoaded] = useState(false);
 
+  const CHAT = useFetchIdAndUpdateSignalR(32) || {};
+
+  const extractValue = (data) => {
+    if (data && data.data && data.data.valor) {
+      return data.data.valor;
+    } else if (data && data.Valor) {
+      return data.Valor;
+    } else {
+      return "";
+    }
+  };
+
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://cdn.bitrix24.es/b26751413/crm/site_button/loader_1_394x0x.js?" + (Date.now() / 60000 | 0);
-    script.async = true;
+    const url = extractValue(CHAT);
+    console.log("URL generada:", url);
 
-    script.onload = () => {
-      setButtonLoaded(true);
-    };
+    // Validación: Solo crea el script si la URL es válida
+    if (url) {
+      const script = document.createElement("script");
+      script.src = `${url}?` + (Date.now() / 60000 | 0);
+      script.async = true;
 
-    document.head.appendChild(script);
+      script.onload = () => {
+        setButtonLoaded(true);
+      };
 
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, []);
+      document.head.appendChild(script);
+
+      return () => {
+        document.head.removeChild(script);
+      };
+    } else {
+      console.error("No se pudo cargar el script. URL inválida.");
+    }
+  }, [CHAT]);
 
   if (!buttonLoaded) {
     return <div>Cargando botón...</div>;
