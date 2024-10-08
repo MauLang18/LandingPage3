5e526cf35921392a249602a21e29cc63649b7e0c
MauLang18 <maulangbonilla.18@gmail.com>
2024-10-08 16:26:40 -0600

    feat: delete whatsapp buttom

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
       <ContactUs />
       {/* <Descarga /> */}
       <Bitrix24Button />
-      <WhatsAppButton />
+      {/* <WhatsAppButton /> */}
       <Footer />
     </>
   );
